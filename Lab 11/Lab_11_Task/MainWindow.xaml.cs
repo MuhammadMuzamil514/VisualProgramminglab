ca156fe3007f2236fa66a5a75212d3d393dae299
MuhammadMuzamil514 <233514@students.au.edu.pk>
2024-12-10 15:38:42 +0500

    Add files via upload

---
+++ Lab 11/Lab_11_Task/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApp5
+{
+    public partial class BringBackPocoyo : Window
+    {
+        public BringBackPocoyo()
+        {
+            InitializeComponent();
+        }
+    }
+}
